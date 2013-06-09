9fbf1736490a98e1137c9d28eb474232ec1bf5f9
Hugh Stephens <hughstephens@hughstephens.com>
2013-06-09 13:10:21 +1000

    fixed startup issue

--- gapipy-withGA.py
+++ gapipy-withGA.py
@@ -11,7 +11,7 @@ for i in range(23,26):
     GPIO.add_event_detect(i, GPIO.RISING, bouncetime=200)  #Defines an event 
 
 # Print Instructions to user
-print("ready for input")
+raw_input("When ready hit enter.\n")
 
 # This setting isn't used
 app_name = "coffeemachine"
@@ -27,10 +27,7 @@ event_category="coffee"
 while True:
     if GPIO.event_detected(23): #calls based on add_event_detect -- you could run a callback in add_event_detect to do more
         tracker.track_event(event_category,"make with 23",label="machine",value=5) #category, action, label, value
-	print("sent 23")
     if GPIO.event_detected(24):
         tracker.track_event(event_category,"make with 24",label="machine",value=5) #category, action, label, value
-	print("sent 24")
     if GPIO.event_detected(25):
         tracker.track_event(event_category,"make with 25",label="machine",value=5) #category, action, label, value
-	print("sent 25")

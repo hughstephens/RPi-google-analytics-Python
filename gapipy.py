e6adb893ddaaddd4828e5143145328ec12a64f5e
Hugh Stephens <hughstephens@hughstephens.com>
2013-06-09 12:58:52 +1000

    modified:   gapipy.py

--- gapipy.py
+++ gapipy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2.7
 # Based on Alex Eames' python scripts for gertboard, see http://RasPi.TV
-# uses 'pyus' project from regisd/pyus as basis for pushing calls to GA
 
 import RPi.GPIO as GPIO
 from pyus import AppTracker
@@ -11,13 +10,24 @@ for i in range(23,26):
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up for switches to1 be detected on gertboard
     GPIO.add_event_detect(i, GPIO.RISING, bouncetime=200)  #Defines an event 
 
-
+# Print Instructions to user
 raw_input("When ready hit enter.\n")
+
+# This setting isn't used
+app_name = "coffeemachine"
+
+# Insert your Tracking ID below, something like UA-XXXXXXX-Y
+tracking_id="UA-41589819-1"
+
+tracker = AppTracker(app_name,tracking_id)
+
+#What event are we tracking?
+event_category="coffee"
 
 while True:
     if GPIO.event_detected(23): #calls based on add_event_detect -- you could run a callback in add_event_detect to do more
-        AppTracker("coffeemachine","UA-41589819-1").track_event("coffee","make",label="machine",value=1)
+        tracker.track_event(event_category,"make with 23",label="machine",value=5) #category, action, label, value
     if GPIO.event_detected(24):
-        print('something 24')
+        tracker.track_event(event_category,"make with 24",label="machine",value=5) #category, action, label, value
     if GPIO.event_detected(25):
-        print('something 25')
+        tracker.track_event(event_category,"make with 25",label="machine",value=5) #category, action, label, value
